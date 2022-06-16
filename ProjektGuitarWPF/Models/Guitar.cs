a9862e22f588a85c8b7748b4a79f92c42e6c33ed
pawelz00 <pawet.wets@gmail.com>
2022-06-16 13:36:07 +0200

    add: models

---
+++ ProjektGuitarWPF/Models/Guitar.cs
@@ -0,0 +1,18 @@
+﻿using guitarapi.Dto;
+
+namespace guitarapi.Models
+{
+    public class Guitar
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int TypeId { get; set; }
+        public virtual GuitarType Type { get; set; }
+        public int StringsId { get; set; }
+        public virtual Strings Strings { get; set; }
+        public int ProducerId { get; set; }
+        public virtual Producer Producer { get; set; }
+        public ICollection<GuitaristGuitar> GuitaristsGuitars { get; set; }
+    }
+}
---
+++ ProjektGuitarWPF/Models/Guitarist.cs
@@ -0,0 +1,10 @@
+﻿namespace guitarapi.Models
+{
+    public class Guitarist
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public ICollection<GuitaristGuitar> GuitaristsGuitars { get; set; }
+    }
+}
